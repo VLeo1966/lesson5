07826e74b3e5c50024de38d9413f39ed21eb2c1a
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-07-11 21:02:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-class Warrior ():
-    def init(self, name, power, endurance, hair_color):
+
+class Warrior():
+    def __init__(self, name, power, endurance, hair_color):
         self.name = name
         self.power = power
         self.endurance = endurance
@@ -7,21 +8,40 @@ class Warrior ():
 
     def sleep(self):
         print(f"{self.name} лег спать")
-        self.endurance = + 2
+        self.endurance += 2
 
     def eat(self):
         print(f"{self.name} сел кушать")
-        self.power = + 1
+        self.power += 1
 
     def hit(self):
         print(f"{self.name} бьет кого-то")
-        self.endurance = - 6
+        self.endurance -= 6
 
     def walk(self):
         print(f"{self.name} гуляет")
 
-    def walk(self):
+    def info(self):
         print(f"имя воина - {self.name}")
         print(f"цвет волос воина - {self.hair_color}")
         print(f"сила воина - {self.power}")
         print(f"выносливость воина - {self.endurance}")
+
+# Правильный способ создания экземпляров класса
+war1 = Warrior(name="Степа", power=76, endurance=54, hair_color="коричневый")
+war1.info()
+war1.sleep()
+war1.eat()
+war1.hit()
+war1.walk()
+war1.info()
+
+war2 = Warrior(name="Егор", power=45, endurance=23, hair_color="блондин")
+war2.info()
+war2.sleep()
+war2.eat()
+war2.hit()
+war2.walk()
+war2.info()
+
+
